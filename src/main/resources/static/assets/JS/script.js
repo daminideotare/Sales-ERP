9e15b462e9660aab18c9c1b58dce7717c1354480
daminideotare <daminideotare@gmail.com>
2026-01-02 12:47:20 +0530

    Update script.js

--- src/main/resources/static/assets/JS/script.js
+++ src/main/resources/static/assets/JS/script.js
@@ -1,5 +1,5 @@
 // ------------ BASE URL -------------
-const BASE = "https://sales-erp-production.up.railway.app/api/v1/dashboard";
+const BASE = "https://sales-erp-production-c47d.up.railway.app/api/v1/dashboard";
 
 let salesChart = null;
 let topPerformanceChart = null;
